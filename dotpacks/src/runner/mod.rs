061bad12bd898ae899591e417ab32b9ae61d7d6b
AsPulse <aspulse.x@gmail.com>
2024-01-01 13:34:19 +0900

    Add runner

---
+++ dotpacks/src/runner/mod.rs
@@ -0,0 +1,9 @@
+#[derive(Debug, Clone)]
+pub struct DotpacksRunner {
+    config: Dotpacks,
+}
+
+/// Holding all information about the dotpacks configuration, that is,
+/// anything you want to manage with dotpacks.
+#[derive(Debug, Clone)]
+pub struct Dotpacks {}
